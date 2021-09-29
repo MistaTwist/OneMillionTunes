1abcd4cc34c195d9c7eec969ed8057d11b1a2a08
Paul Perry <porlyp@gmail.com>
2021-09-29 21:19:57 +0100

    Added rudimentary spotify share link validation

--- src/components/SubmitTune.svelte
+++ src/components/SubmitTune.svelte
@@ -5,14 +5,23 @@ import { submittedTunes } from '../stores/tuneStore';
     let id = Math.max.apply(Math, $submittedTunes.map(function(o) { return o.id; }));
     return id ;
   }
-//"https://open.spotify.com/embed/track/3vYxoh9Y7L5Tdr0mhaAaUu?theme=0"
-	function validateIdThenAdd(spotifyLink) {
-		let trackId = spotifyLink.toString().split("/")[5].split('?')[0];
-		console.log(trackId);
-		//add(trackId);
+
+	function validateIdThenAdd(input) {
+		let trackId = '';
+		let spotifyLink = input.value;
+		let sLink = spotifyLink.toString();
+
+		if(sLink.startsWith('https://open.spotify.com/track/')){
+			trackId = sLink.split("https://open.spotify.com/track/")[1].toString().split('?')[0]
+		}
+
+		if(trackId.length > 0) {
+			add(trackId);
+		}
+		input.value = '';
 	}
 
- function add(input) {
+ function add(trackId) {
     // get max id to ensure we don't repeat/miss any ids out.
     let newId = getMaxId() + 1;
     console.log(newId);
@@ -21,13 +30,12 @@ import { submittedTunes } from '../stores/tuneStore';
 			id: newId, 
 			hasBeenReviewed: false, 
 			submittedBy: 'TBC', 
-			spotifyUrl: input.value,
+			spotifyUrl: trackId,
 			isLiked: null,
 			dateSubmitted: new Date().toLocaleDateString()
 		};
 
 		$submittedTunes = [tune, ...$submittedTunes];
-		input.value = '';
 	}
 </script>
 
@@ -38,7 +46,7 @@ import { submittedTunes } from '../stores/tuneStore';
 	</h2>
 	<input type="text" 
 	 placeholder="paste a spotify link or track id here"
-	 on:keydown={e => e.key === 'Enter' && validateIdThenAdd(e.target.value.toString())}
+	 on:keydown={e => e.key === 'Enter' && validateIdThenAdd(e.target)}
 	/>
 
 </main>
