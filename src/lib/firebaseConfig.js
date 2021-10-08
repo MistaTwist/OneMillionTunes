d94210eed9ebed0580e24949a014d25d377edffc
Paul Perry <porlyp@gmail.com>
2021-10-08 13:19:20 +0100

    removed keys
    dude, learn to gitignore better!

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -1,9 +1,3 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBwNdr8LYCr54KpoFqL7WiwQwdsA5S4Wf0",
-  authDomain: "onemilliontunes-916e3.firebaseapp.com",
-  projectId: "onemilliontunes-916e3",
-  databaseURL: "https://onemilliontunes-916e3.firebaseio.com",
-  storageBucket: "onemilliontunes-916e3.appspot.com",
-  messagingSenderId: "520288369644",
-  appId: "1:520288369644:web:97c689d50c0d112b155ec9"
+
 };
